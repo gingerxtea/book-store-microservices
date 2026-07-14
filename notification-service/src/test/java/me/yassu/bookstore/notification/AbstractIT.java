fa22bbce76dc8081a5fecead77b189d12ecba049
gingerxtea <yassu7221@gmail.com>
2026-07-15 00:49:17 +0530

    changed notifications tests config

--- notification-service/src/test/java/me/yassu/bookstore/notification/AbstractIT.java
+++ notification-service/src/test/java/me/yassu/bookstore/notification/AbstractIT.java
@@ -3,6 +3,6 @@ package me.yassu.bookstore.notification;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(NotificationServiceApplication.class)
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIT {}
